9d93046e7ad9e120bf38a2de9e033adeb6aa863d
GAURAV KUMAR <78220135+gauravkumar62024@users.noreply.github.com>
2021-02-24 23:52:13 +0530

    Add files via upload

---
+++ insertionsort_inst_conting.cpp
@@ -0,0 +1,53 @@
+#include <math.h>
+#include <stdio.h>
+int static count;
+void Insertion_Sort(int arr[], int n)
+{
+	int i,j,key;
+	for (i= 1; i < n; i++) 
+	{
+		key = arr[i];
+		j = i- 1;
+		count=count+3;
+		while (j >= 0 && arr[j] > key) 
+		{
+
+			arr[j + 1] = arr[j];
+         	j = j - 1;
+          count=count+3;
+		}
+          count++;
+	    	arr[j + 1] = key;
+	    	++count;
+
+	} count++;
+
+}
+
+void Print_Array(int arr[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		printf("%d ", arr[i]);
+	printf("\n");
+}
+
+int main()
+{
+    int n,i ;
+    int A[100];
+
+    printf("please enternumber of element to be entered: ");
+   scanf("%d",&n);
+
+   printf("Enter %d elements: ", n);
+   for(i=0;i<n;i++)
+      scanf("%d",&A[i]);
+
+	Insertion_Sort(A, n);
+	Print_Array(A, n);
+	printf("number of instrution executeed is %d",count);
+
+	return 0;
+}
+
